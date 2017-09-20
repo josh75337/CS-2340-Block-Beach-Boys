971af80a0035fba026fdab6139dfe0e6330ef428
eli1797 <elibailey97@gmail.com>
2017-09-20 16:59:02 -0400

    Changed Theme
    Changed the bottom navbar theme to blue when open

--- AndroidStudioProjects/app/src/main/java/com/ohrats/bbb/ohrats/LoginActivity.java
+++ AndroidStudioProjects/app/src/main/java/com/ohrats/bbb/ohrats/LoginActivity.java
@@ -46,7 +46,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     /**
      * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
+     * @TODO: remove after connecting to a real authentication system.
      * Credentials are added in the form "username:password"
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{"user:pass"};
