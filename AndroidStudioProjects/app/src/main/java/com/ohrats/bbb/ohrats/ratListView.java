443027a42de5c6b87200bf28971a3368b299948d
Justin Weltner-Evans <jtwe3@gatech.edu>
2017-10-06 14:47:47 -0400

    Rat List View

---
+++ AndroidStudioProjects/app/src/main/java/com/ohrats/bbb/ohrats/ratListView.java
@@ -0,0 +1,24 @@
+package com.ohrats.bbb.ohrats;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * Created by Justin on 10/4/2017.
+ */
+
+public class ratListView extends Activity{
+    // Array of strings...
+    ListView simpleList;
+    String countryList[] = {"India", "China", "australia", "Portugle", "America", "NewZealand"};
+
+    @Override   protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rat_listview);
+        simpleList = (ListView)findViewById(R.id.rat_reports);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_rat_listview, R.id.textView, countryList);
+        simpleList.setAdapter(arrayAdapter);
+    }
+}
