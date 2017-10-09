5efd5753c2ef83751bed87a2abd9d515400f5f53
Justin Weltner-Evans <jtwe3@gatech.edu>
2017-10-09 15:59:31 -0400

    List View works. Needs data read in and onClicks for reports.  Edit:Trying to implement Firebase for ListView Edit 2: Firebase client crashes code/ do not know why

--- AndroidStudioProjects/app/src/main/java/com/ohrats/bbb/ohrats/ViewRatReportListActivity.java
+++ AndroidStudioProjects/app/src/main/java/com/ohrats/bbb/ohrats/ViewRatReportListActivity.java
@@ -8,8 +8,8 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.firebase.client.Firebase;
-import com.firebase.ui.database.FirebaseListAdapter;
+//import com.firebase.client.Firebase;
+//import com.firebase.ui.database.FirebaseListAdapter;
 //import com.google.firebase.auth.FirebaseAuth;
 //import com.google.firebase.auth.FirebaseUser;
 //import com.firebase.ui.database.FirebaseRecyclerAdapter;
@@ -23,28 +23,28 @@ public class ViewRatReportListActivity extends Activity{
     // Array of strings...
     ListView simpleList;
     String countryList[] = {"India", "China", "australia", "Portugle", "America", "NewZealand"};
-
-    Firebase mRef;
-    FirebaseListAdapter<String> mAdapter;
+//
+//    Firebase mRef;
+//    FirebaseListAdapter<String> mAdapter;
 
     @Override   protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_rat_report_list);
         simpleList = (ListView)findViewById(R.id.rat_reports);
-        //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_rat_listview, R.id.textView, countryList);
-        //simpleList.setAdapter(arrayAdapter);
-        mRef = new Firebase("https://androidstudioprojects-b4132.firebaseio.com/");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_rat_listview, R.id.textView, countryList);
+        simpleList.setAdapter(arrayAdapter);
+//        mRef = new Firebase("https://androidstudioprojects-b4132.firebaseio.com/");
 //        mListView = (ListView) findViewById(R.id.ListView);
-        mAdapter = new FirebaseListAdapter<RatSighting>(this, RatSighting.class, R.layout.activity_rat_listview, mRef) {
-            @Override
-            protected void populateView(View view, RatSighting ratSighting, int position) {
-                //Set the value for the views
-                ((TextView)view.findViewById(R.id.xxx)).setText(ratSighting.getName());
-                //text.set(s);
-                //...
-            }
-        };
-        simpleList.setAdapter(mAdapter);
+//        mAdapter = new FirebaseListAdapter<RatSighting>(this, RatSighting.class, R.layout.activity_rat_listview, mRef) {
+//            @Override
+//            protected void populateView(View view, RatSighting ratSighting, int position) {
+//                //Set the value for the views
+//                ((TextView)view.findViewById(R.id.xxx)).setText(ratSighting.getName());
+//                //text.set(s);
+//                //...
+//            }
+//        };
+//        simpleList.setAdapter(mAdapter);
     }
 
 //
