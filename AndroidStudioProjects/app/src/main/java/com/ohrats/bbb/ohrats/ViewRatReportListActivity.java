58566927ca9dc107b7385049871f5b5f3ebc520e
Krupczak, Matthew William <mkupczak3@gatech.edu>
2017-10-10 23:36:10 -0400

    cleaned up ViewRatReportListActivity and tested

--- AndroidStudioProjects/app/src/main/java/com/ohrats/bbb/ohrats/ViewRatReportListActivity.java
+++ AndroidStudioProjects/app/src/main/java/com/ohrats/bbb/ohrats/ViewRatReportListActivity.java
@@ -9,7 +9,6 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -17,12 +16,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
-
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 //import com.firebase.client.Firebase;
 //import com.firebase.ui.database.FirebaseListAdapter;
@@ -44,6 +39,7 @@ public class ViewRatReportListActivity extends Activity{
     Button mAddSightingButton;
 
     private DatabaseReference mDatabase;
+    private final int SIGHTINGS_PER_PAGE = 50;
 
     int count;
 
@@ -58,41 +54,40 @@ public class ViewRatReportListActivity extends Activity{
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        count = 10;
-
-//        RatSighting r0 = new RatSighting("0", null, "Georgia Tech Dorm", "30332", "NO", "Definitely not Atlanta", "Maybe Georgia", 8.1, 10.1);
-//        RatSighting r1 = new RatSighting("1");
-//        RatSighting r2 = new RatSighting("2");
-//        RatSighting r3 = new RatSighting("3");
-//
-//        sightingList.add(r0);
-//        sightingList.add(r1);
-//        sightingList.add(r2);
-//        sightingList.add(r3);
+        // Populates sightingList asynchronously,
+        //     i.e. We can't have our final list view until it finishes populating
         updateSightingList();
-        Log.v(TAG, "sightingList.size is: " + sightingList.size());
-        for(int i = 0; i < sightingList.size(); i++){
-            Log.d(TAG, "Element no " + i + " of sightingList is: " + sightingList.get(i).toString());
-        }
 
+        // Button for going to the manual add and csv selector screen
         mAddSightingButton = (Button) findViewById(R.id.raddsighting);
         mAddSightingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                viewAddRatSightingPage();
+                viewAddSightingActivity();
             }
         });
     }
 
-    private void viewAddRatSightingPage() {
+    /**
+     * Switches to the AddSightingActivity
+     */
+    private void viewAddSightingActivity() {
         Intent inView = new Intent(ViewRatReportListActivity.this, AddSightingActivity.class);
         startActivity(inView);
     }
 
+    /**
+     * updates the ListView with the current contents of sightingList (i.e. RatSighting(s))
+     */
     private void updateListView() {
         Log.v(TAG, "updateListView called while size of sightingList is" + sightingList.size());
         simpleList = (ListView) findViewById(R.id.rat_reports);
-        ArrayAdapter<RatSighting> arrayAdapter = new ArrayAdapter<>(this, R.layout.activity_rat_listview, R.id.textView, sightingList);
+        ArrayAdapter<RatSighting> arrayAdapter =
+                new ArrayAdapter<>(this,
+                        R.layout.activity_rat_listview,
+                        R.id.textView,
+                        sightingList);
+        //---------------------------------------------
         simpleList.setAdapter(arrayAdapter);
         simpleList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -105,20 +100,32 @@ public class ViewRatReportListActivity extends Activity{
 
     }
 
+    /**
+     * fetches RatSighting(s) from database and adds them to sightingList
+     *
+     * also calls updateListView() every time a RatSighting is fetched in order to populate
+     *     the list view
+     */
     private void updateSightingList() {
         Log.v(TAG, "updateSightingList called" );
         DatabaseReference sightingsRef = mDatabase.child("sightings");
 
-        Query query = sightingsRef.orderByKey().limitToLast(50);
+        Query query = sightingsRef.orderByKey().limitToLast(SIGHTINGS_PER_PAGE);
         query.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 Log.v(TAG, "onChildAdded event" );
-                Log.v(TAG, "UID of child sighting: " + dataSnapshot.getValue(RatSighting.class).getKey());
-                Log.v(TAG, "Date of child sighting: " + dataSnapshot.getValue(RatSighting.class).getDate());
+                Log.v(TAG, "UID of child sighting: "
+                        + dataSnapshot.getValue(RatSighting.class).getKey());
+                //-----------------------------------------------------------
+                Log.v(TAG, "Date of child sighting: "
+                        + dataSnapshot.getValue(RatSighting.class).getDate());
+                //-----------------------------------------------------------
+
                 sightingList.addFirst(dataSnapshot.getValue(RatSighting.class));
-                Log.v(TAG, "size of sightingList after adding: " + sightingList.size());
                 updateListView();
+
+                Log.v(TAG, "size of sightingList after adding: " + sightingList.size());
             }
 
             @Override
@@ -141,7 +148,5 @@ public class ViewRatReportListActivity extends Activity{
 
             }
         });
-        updateListView();
-        count += 10;
     }
 }
