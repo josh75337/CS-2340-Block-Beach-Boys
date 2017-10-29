9172c577eea292358253b06a8eba5f0452b0cb30
eli1797 <elibailey97@gmail.com>
2017-10-29 17:41:57 -0400

    added charts activity

---
+++ AndroidStudioProjects/app/src/main/java/com/ohrats/bbb/ohrats/ChartsActivity.java
@@ -0,0 +1,13 @@
+package com.ohrats.bbb.ohrats;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChartsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_charts);
+    }
+}
