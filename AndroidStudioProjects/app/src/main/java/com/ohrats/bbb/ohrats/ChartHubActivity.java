104284ede468cbaf682844e20119d7dcafd831a9
eli1797 <elibailey97@gmail.com>
2017-10-30 02:28:23 -0400

    Almost to finished m9. Need to debug exception in XY Default Chart.
    Also must figure out how to communicate months if the time span is greater than a year. Current code for months in dummy code to be changed later from YYYY-MM to whatever we decide

--- AndroidStudioProjects/app/src/main/java/com/ohrats/bbb/ohrats/ChartHubActivity.java
+++ AndroidStudioProjects/app/src/main/java/com/ohrats/bbb/ohrats/ChartHubActivity.java
@@ -3,44 +3,54 @@ package com.ohrats.bbb.ohrats;
 import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.DashPathEffect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.androidplot.util.PixelUtils;
-import com.androidplot.xy.CatmullRomInterpolator;
-import com.androidplot.xy.LineAndPointFormatter;
-import com.androidplot.xy.SimpleXYSeries;
-import com.androidplot.xy.XYGraphWidget;
-import com.androidplot.xy.XYPlot;
-import com.androidplot.xy.XYSeries;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
-import java.text.FieldPosition;
-import java.text.Format;
-import java.text.ParsePosition;
-import java.util.Arrays;
+import java.lang.reflect.Array;
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
 
+/**
+ * Created by Eli Bailey
+ * Intended as a hub for charts
+ * This is the last class that should handle rat sightings and dates. All the charts should be
+ * passed simpler information
+ */
 public class ChartHubActivity extends AppCompatActivity {
 
     private static final String TAG = "ChartHubActivity";
 
-    //spinner to selection date range: Monthly, Yearly, etc.
+    private final int SIGHTINGS_PER_PAGE = 50;
+
+    //spinner to select the type of chart
     private Spinner typeSpinner;
+    private Spinner timeSpinner;
+
+    private DatabaseReference mDatabase;
 
     private Button mLaunchChartButton;
+
+    //Arraylist for keeping ratsightings.
+    private ArrayList<RatSighting> sightingList = new ArrayList<>();
 
     //date range
     private TextView mStartDate;
@@ -59,12 +69,20 @@ public class ChartHubActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_charts);
 
+        //get firebase reference
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
         typeSpinner = (Spinner) findViewById(R.id.type_spinner);
+        timeSpinner = (Spinner) findViewById(R.id.time_spinner);
 
         //Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.chart_type_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         typeSpinner.setAdapter(adapter);
+
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.chart_time_array, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        timeSpinner.setAdapter(adapter1);
 
         //default times
         startDateMillis = 0;
@@ -97,10 +115,14 @@ public class ChartHubActivity extends AppCompatActivity {
                 chartStart = new GregorianCalendar(year, month, dayOfMonth).getTimeInMillis();
                 month = month + 1;
                 mStartDate.setText(String.format("%02d/%02d/%4d", month, dayOfMonth, year));
-                getCurrentFocus().clearFocus();
-                mLaunchChartButton.setEnabled(true);
-                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                try {
+                    getCurrentFocus().clearFocus();
+                    mLaunchChartButton.setEnabled(true);
+                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                } catch (NullPointerException npe) {
+                    Log.d(TAG, "Caught null pointer exception");
+                }
             }
         };
 
@@ -128,10 +150,14 @@ public class ChartHubActivity extends AppCompatActivity {
                 chartEnd = new GregorianCalendar(year, month, dayOfMonth + 1).getTimeInMillis() - 1;
                 month = month + 1;
                 mEndDate.setText(String.format("%02d/%02d/%4d", month, dayOfMonth, year));
-                getCurrentFocus().clearFocus();
-                mLaunchChartButton.setEnabled(true);
-                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                try {
+                    getCurrentFocus().clearFocus();
+                    mLaunchChartButton.setEnabled(true);
+                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                } catch (NullPointerException npe) {
+                    Log.d(TAG, "Caught null pointer exception");
+                }
             }
         };
 
@@ -152,21 +178,47 @@ public class ChartHubActivity extends AppCompatActivity {
 
     private void startCreateChart() {
         //determine what type of chart the user wants
-        String chartType = (String) typeSpinner.getSelectedItem();
+        final String chartType = (String) typeSpinner.getSelectedItem();
+        final String chartTime = (String) timeSpinner.getSelectedItem();
+        Boolean isYears = chartTime.equals("Yearly");
 
         //create a title based of of the chart
         String title = "Sighting by Month and Year";
         switch (chartType) {
-            case "XY Plot": title = title + ": XY Plot";
-                break;
-            default: chartType = "XY Plot";
+//            case "XY Plot": title = title + ": XY Plot";
+//                break;
+            default: title = title + ": XY Plot";
                 break;
         }
 
-        createChart(title, chartType);
+        //call a query that gets the rat sightings within the range and puts them in an arraylist
+        ArrayList<RatSighting> rangedSightings = getRangedSightings();
+
+        HashMap<String, Integer> sortedTimeFrame = null;
+        if (isYears) {
+            sortedTimeFrame = sortByYear(rangedSightings);
+        } else if (chartTime.equals("Monthly")) {
+            sortedTimeFrame = sortByMonth(rangedSightings);
+        } else {
+            Log.d(TAG, "Charttime was invalid, not meeting any expected values");
+            //@TODO: maybe throw a pop-up and make them restart
+        }
+
+
+        try {
+            createChart(title, chartType, sortedTimeFrame, isYears);
+        } catch (NullPointerException npe) {
+            Log.d(TAG, "sortedTimeFrame may have been null");
+        }
     }
 
-    private void createChart(String title, String chartType) {
+    /**
+     * Creates an intent and populated a chart activity before starting that activity
+     * @param title The desired title of the chart @TODO: not working yet
+     * @param chartType The type of chart desired (XY Plot, histogram, etc)
+     * @param inputData The data from firebase
+     */
+    private void createChart(String title, String chartType, HashMap<String, Integer> inputData, Boolean isYears) {
         Log.v(TAG, "Called createChart()");
 
         //create a default to the xy plot
@@ -178,14 +230,153 @@ public class ChartHubActivity extends AppCompatActivity {
                 Log.v(TAG, "creating histogram");
                 break;
             default:
+                Log.v(TAG, "creating xy plot");
                 //this is the XYDefaultPlot
                 break;
         }
+        if (isYears) {
+            in.putExtra("XYSERIES_TITLE", "Yearly");
+        } else {
+            in.putExtra("XYSERIES_TITLE", "Monthly");
+        }
 
+        ArrayList<String> domain = new ArrayList<>();
+        ArrayList<Integer> range = new ArrayList<>();
         //setup the plot with the user selected values
+        if (!inputData.isEmpty()) {
+            domain = new ArrayList<>(inputData.keySet());
+            range = new ArrayList<>(inputData.values());
+        }
 
+        ArrayList<Integer> xVals = new ArrayList<>(domain.size());
+        for (int i = 0; i < domain.size(); i++) {
+            String cur = domain.get(i);
+            xVals.add(Integer.parseInt(cur));
+        }
+
+        //pass data with the intent
+        in.putExtra("X_VALS", xVals);
+        in.putExtra("Y_VALS", range);
 
         //navigate the the plot
         startActivity(in);
+    }
+
+    private ArrayList<RatSighting> getRangedSightings(){
+        DatabaseReference sightingsRef = mDatabase.child("sightings");
+
+        //reset the arraylist of rat-sightings
+        sightingList = new ArrayList<>();
+
+        Query query = sightingsRef.orderByChild("date")
+                .startAt(DateStandardsBuddy.getISO8601MINStringForDate(new Date(startDateMillis)))
+                .endAt(DateStandardsBuddy.getISO8601MAXStringForDate(new Date(endDateMillis))).limitToLast(SIGHTINGS_PER_PAGE);
+        query.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                // updates arraylist to include added data
+                sightingList.add(dataSnapshot.getValue(RatSighting.class));
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {}
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                sightingList.remove(dataSnapshot.getValue(RatSighting.class));
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {}
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        });
+
+        return sightingList;
+    }
+
+    /**
+     * Count the sightings and sort them into a HashMap
+     *
+     * I'm making the assumption here that the ArrayList is inorder by date from earliest to latest
+     *
+     * @param inputList An arraylist of rat sightings
+     * @return A hashmap with years as keys and integers for how many sightings per year
+     */
+    private HashMap<String, Integer> sortByYear(ArrayList<RatSighting> inputList) {
+        HashMap<String, Integer> toReturn = new HashMap<>();
+        String year = null;
+        int yearCount = 0;
+        for (int i = 0; i < inputList.size(); i++) {
+            //this may be my friend here getISO8601ESTSansDayStringForDate()
+            try {
+                Date aDate = DateStandardsBuddy.getDateFromISO8601ESTString(inputList.get(0).getDate());
+                String isoDate = DateStandardsBuddy.getISO8601ESTSansDayStringForDate(aDate);
+                String[] isoSplit = isoDate.split("-");
+                String rYear = isoSplit[0];
+
+                if (year.equals(null)) {
+                    year = rYear;
+                    yearCount++;
+                } else if (!(year.equals(rYear))) {
+                    Log.v(TAG, rYear.toString());
+                    //the year has changed
+                    toReturn.put(year, yearCount);
+                    yearCount = 0;
+                    //update the year
+                    year = rYear;
+                } else {
+                    yearCount++;
+                }
+
+            } catch (ParseException pe) {
+                Log.d(TAG, "Parse Exception in sortByYear");
+            } catch (Exception e) {
+                Log.d(TAG, "Date from sighting caused exception in sortByYear");
+            }
+        }
+        return toReturn;
+    }
+
+    /**
+     * Count the sightings and sort them into a HashMap
+     *
+     * I'm making the assumption here that the ArrayList is inorder by date from earliest to latest
+     *
+     * @param inputList An arraylist of rat sightings
+     * @return A hashmap with months as keys and integers for how many sightings per month
+     */
+    private HashMap<String, Integer> sortByMonth(ArrayList<RatSighting> inputList) {
+        // need to count the amount of sightings per year/per month
+        HashMap<String, Integer> toReturn = new HashMap<>();
+        String month = null;
+        int monthCount = 0;
+        for (int i = 0; i < inputList.size(); i++) {
+            try {
+                Date aDate = DateStandardsBuddy.getDateFromISO8601ESTString(inputList.get(0).getDate());
+                String isoDate = DateStandardsBuddy.getISO8601ESTSansDayStringForDate(aDate);
+
+                if (month.equals(null)) {
+                    month = isoDate;
+                    monthCount++;
+                } else if (!(month.equals(isoDate))) {
+                    Log.v(TAG, isoDate.toString());
+                    toReturn.put(month, monthCount);
+                    monthCount = 0;
+                    month = isoDate;
+                } else {
+                    monthCount++;
+                }
+
+            } catch (ParseException pe) {
+                Log.d(TAG, "Parse Exception in sortByMonth");
+            } catch (Exception e) {
+                Log.d(TAG, "Date from sighting caused exception in sortByMonth");
+            }
+        }
+
+        return toReturn;
     }
 }
--- AndroidStudioProjects/app/src/main/java/com/ohrats/bbb/ohrats/XYDefaultPlot.java
+++ AndroidStudioProjects/app/src/main/java/com/ohrats/bbb/ohrats/XYDefaultPlot.java
@@ -4,9 +4,8 @@ import android.graphics.Color;
 import android.graphics.DashPathEffect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-import com.androidplot.util.PixelUtils;
-import com.androidplot.xy.CatmullRomInterpolator;
 import com.androidplot.xy.LineAndPointFormatter;
 import com.androidplot.xy.SimpleXYSeries;
 import com.androidplot.xy.XYGraphWidget;
@@ -16,53 +15,62 @@ import com.androidplot.xy.XYSeries;
 import java.text.FieldPosition;
 import java.text.Format;
 import java.text.ParsePosition;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class XYDefaultPlot extends AppCompatActivity {
 
     private XYPlot plot;
 
+    private Number[] xVals;
+
+    private Number[] yVals;
+
+    private String timeframe;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_xyplot);
 
+        ArrayList<Integer> domain = getIntent().getIntegerArrayListExtra("X_VALS");
+        xVals = new Number[domain.size()];
+        for (int j = 0; j < domain.size(); j++) {
+            xVals[j] = domain.get(j);
+        }
+
+        ArrayList<Integer> range = getIntent().getIntegerArrayListExtra("Y_VALS");
+        yVals = new Number[range.size()];
+        for (int j = 0; j < range.size(); j++) {
+            yVals[j] = range.get(j);
+        }
+        timeframe = getIntent().getStringExtra("XYSERIES_TITLE");
+
         // initialize our XYDefaultPlot reference:
         plot = (XYPlot) findViewById(R.id.plot);
 
         // create a couple arrays of y-values to plot:
-        final Number[] domainLabels = {1, 2, 3, 6, 7, 8, 9, 10, 13, 14};
-        Number[] series1Numbers = {1, 4, 2, 8, 4, 16, 8, 32, 16, 64};
-        Number[] series2Numbers = {5, 2, 10, 5, 20, 10, 40, 20, 80, 40};
+        final Number[] domainLabels = xVals;
+        Number[] series1Numbers = yVals;
 
         // turn the above arrays into XYSeries':
         // (Y_VALS_ONLY means use the element index as the x value)
         XYSeries monthly = new SimpleXYSeries(
-                Arrays.asList(series1Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Monthly");
-        XYSeries yearly = new SimpleXYSeries(
-                Arrays.asList(series2Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Yearly");
+                Arrays.asList(series1Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, timeframe);
 
         // create formatters to use for drawing a series using LineAndPointRenderer
         // and configure them from xml:
         LineAndPointFormatter series1Format = new LineAndPointFormatter(Color.BLUE, Color.WHITE, null, null);
 
-        LineAndPointFormatter series2Format = new LineAndPointFormatter(Color.GREEN, Color.WHITE, null, null);
-
-        // add an "dash" effect to the series2 line:
-        series2Format.getLinePaint().setPathEffect(new DashPathEffect(new float[] {
-
-                // always use DP when specifying pixel sizes, to keep things consistent across devices:
-                PixelUtils.dpToPix(20),
-                PixelUtils.dpToPix(15)}, 0));
-
 
         // add a new series' to the xyplot:
         plot.addSeries(monthly, series1Format);
-        plot.addSeries(yearly, series2Format);
 
         plot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).setFormat(new Format() {
             @Override
             public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
+                Log.v("XY Default Plot", obj.toString());
                 int i = Math.round(((Number) obj).floatValue());
                 return toAppendTo.append(domainLabels[i]);
             }
@@ -73,4 +81,5 @@ public class XYDefaultPlot extends AppCompatActivity {
         });
 
     }
+
 }
