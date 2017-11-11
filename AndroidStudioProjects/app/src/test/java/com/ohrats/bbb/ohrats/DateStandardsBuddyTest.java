7db85d34f3adc62dbf9012f5ae66ae5aefe050b8
eli1797 <elibailey97@gmail.com>
2017-11-10 20:36:59 -0500

    Created jUnit test

---
+++ AndroidStudioProjects/app/src/test/java/com/ohrats/bbb/ohrats/DateStandardsBuddyTest.java
@@ -0,0 +1,40 @@
+package com.ohrats.bbb.ohrats;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Elijah on 11/10/2017.
+ */
+public class DateStandardsBuddyTest {
+
+    private DateFormat iso8601DF;
+
+    private Date curDate1;
+
+    private String expected1;
+
+    @Before
+    public void setUp() throws Exception {
+        iso8601DF = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss"); // Quoted "Z" to indicate UTC, no timezone offset
+
+        //date is the epoch
+        curDate1 = new Date(0);
+
+        //epoch local time and date
+        expected1 = "1969-12-31T19:00:00";
+    }
+
+    @Test
+    public void getISO8601ESTStringForDate() throws Exception {
+        String date = DateStandardsBuddy.getISO8601ESTStringForDate(curDate1);
+        assertEquals(expected1, date);
+    }
+
+}
