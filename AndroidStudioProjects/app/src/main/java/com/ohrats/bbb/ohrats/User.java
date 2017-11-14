2feb4970e9679db045484d1af958a4e4d0bf487b
eli1797 <elibailey97@gmail.com>
2017-11-13 20:00:09 -0500

    Getters and setters in user, added javadocs and suppressed

--- AndroidStudioProjects/app/src/main/java/com/ohrats/bbb/ohrats/User.java
+++ AndroidStudioProjects/app/src/main/java/com/ohrats/bbb/ohrats/User.java
@@ -7,6 +7,7 @@ import com.google.firebase.database.IgnoreExtraProperties;
  * POJOs can easily be added to the database
  * Created by Elijah on 9/25/2017.
  */
+@SuppressWarnings("unused") //Getters and setters not used in the codebase but required by Firebase
 @IgnoreExtraProperties
 class User {
 
@@ -16,36 +17,76 @@ class User {
     //Level -> User or Admin
     private String level;
 
-    //Getters and setters required by firebase database
+    /* Getters and setters required by firebase database */
+
+    /**
+     * Getter for the user's email
+     * REQUIRED by Firebase
+     * @return String containing the user's email
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Sets the users email
+     * REQUIRED by Firebase
+     * @param email The new email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Getter for the user's password
+     * REQUIRED by Firebase
+     * @return A string containing the users password
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Setter for password
+     * REQUIRED by Firebase
+     * @param password The new password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * Getter for the users level (User or Admin)
+     * REQUIRED by Firebase
+     * @return A string of the users level
+     */
     public String getLevel() {
         return level;
     }
 
+    /**
+     * Setter for level
+     * REQUIRED by Firebase
+     * @param level the new level
+     */
     public void setLevel(String level) {
         this.level = level;
     }
 
+    /**
+     * Constructor
+     * REQUIRED by Firebase
+     */
     public User() {
         // From Google People @ Firebase
         // "Default constructor required for calls to DataSnapshot.getValue(User.class)"
     }
 
+    /**
+     * Constructor with email and password
+     * @param email The user's email
+     * @param password The user's password
+     */
     public User(String email, String password) {
         this(email, password, "User");
     }
